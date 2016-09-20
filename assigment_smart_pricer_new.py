5f02df7d0ae8c212b99be4cf55a9ed2d33ab9fbd
julira <julira@live.de>
2016-09-20 10:08:20 +0200

    assignment_smart_pricer_new.py

--- assigment_smart_pricer_new.py
+++ assigment_smart_pricer_new.py
@@ -2,14 +2,19 @@ import pandas as pd
 
 with open('C:/Users/Yulia/Desktop/fernstudium/datascientist/python/ds/tickets_sold.csv','rb') as csvfile:
     f = pd.read_csv(csvfile)
-    print f
-  with open('C:/Users/Yulia/Desktop/fernstudium/datascientist/python/ds/row_capacity.csv','rb') as csvfile:
+with open('C:/Users/Yulia/Desktop/fernstudium/datascientist/python/ds/row_capacity.csv','rb') as csvfile:
     a = pd.read_csv(csvfile) 
-    print a
+    
+##  Total_tickets_per_week_movie_row 
+f['tickets']=1   
+x=f.groupby(['movie','calendarweek','auditorium_row'])['tickets'].count()
+print x
+
 ##Seat_load_factor_per_week_movie_row
 x1 = x.to_frame().reset_index()
-print x1
 df=a.merge(x1)
-
+df['load_factor']=df['tickets']/df['max_seats_per_row']
+print df
 #seat_load_factor_per_week_row
+
 df.groupby(['calendarweek','auditorium_row'])['load_factor'].mean()
